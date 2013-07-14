68855cfd4e92185337131efe554b8c13de0e4bab
Jackneill <jackneill1000@gmail.com>
2013-07-14 18:06:24 +0000

    basic comparing system without word (alpha,beta,rc) support)

--- semver.go
+++ semver.go
@@ -1,4 +1,4 @@
-package goversion
+package gosemver
 
 import (
 	"strconv"
@@ -8,7 +8,7 @@ import (
 // Compare 2 version formats for which is greater.
 // For more information see http://semver.org
 // Returns: 0 if equals, 1 if the first version arg is greater, 2 if the second.
-func compareVersions(v1, v2 string) int {
+func Compare(v1, v2 string) int {
 	if strings.Contains(v1, "-") {
 		strings.Replace(v1, "-", ".", -1)
 	}
@@ -18,9 +18,15 @@ func compareVersions(v1, v2 string) int {
 	ver1 := strings.Split(v1, ".")
 	ver2 := strings.Split(v2, ".")
 
-	/*if ver1[0] == ver2[0] {
+	if ver1[0] == ver2[0] {
 		if ver1[1] == ver2[1] && len(ver1) > 1 && len(ver2) > 1 {
-
+			if ver1[2] == ver2[2] && len(ver1) > 2 && len(ver2) > 2 {
+				return 0
+			} else if ver1[2] > ver2[2] {
+				return 1
+			} else {
+				return 2
+			}
 		} else if ver1[1] > ver2[1] {
 			return 1
 		} else {
@@ -30,5 +36,5 @@ func compareVersions(v1, v2 string) int {
 		return 1
 	} else {
 		return 2
-	}*/
+	}
 }
